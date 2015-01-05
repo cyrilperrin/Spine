a4a28a97fc15ea2b324edfb861aa3dec5e8d0965
cyrilperrin <perrin.cyril.88@gmail.com>
2015-01-05 22:21:01 +0100

    Add configuration INI loader test

---
+++ tests/unit/Configuration/Loader/IniTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use CyrilPerrin\Spine\Configuration\Loader\Ini;
+/**
+ * Configuration INI loader test
+ */
+class IniTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Load() test
+     */
+    public function testParseUrl()
+    {
+        $loader = new Ini(__DIR__.'/IniTest.ini');
+        
+        $productionConfiguration = $loader->getData('production');
+        $this->assertEquals(
+            array(
+                'varA' => 'A',
+                'varB' => 'A',
+                'varC' => 'A',
+                'array' => array('A', 'B', 'C')
+            ),
+            $productionConfiguration
+        );
+        
+        $productionConfiguration = $loader->getData('development');
+        $this->assertEquals(
+            array(
+                'varA' => 'A',
+                'varB' => 'B',
+                'varC' => 'B',
+                'array' => array('D', 'E', 'F')
+            ),
+            $productionConfiguration
+        );
+        
+        $productionConfiguration = $loader->getData('integration');
+        $this->assertEquals(
+            array(
+                'varA' => 'A',
+                'varB' => 'A',
+                'varC' => 'C',
+                'varD' => 'C',
+                'array' => array('A', 'B', 'C')
+            ),
+            $productionConfiguration
+        );
+    }
+    
+}
